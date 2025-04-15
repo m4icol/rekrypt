5a2cc851ce09e976bc587440238da2d3d2ee631a
Maickol <m4icol.r12@gmail.com>
2025-04-15 16:59:38 -0500

    fix: scrollbar in mobile

--- frontend/src/sections/Sidebar.tsx
+++ frontend/src/sections/Sidebar.tsx
@@ -33,7 +33,7 @@ function Sidebar({ selectedMethods, onMethodsChange }: SidebarProps) {
       <section
         className={`fixed top-0 left-0 z-30 overflow-x-hidden overflow-y-auto scroll-bar-custom
             bg-sidebar transform transition-transform duration-300 ease-in-out 
-            w-72 lg:w-100 flex flex-col gap-10 lg:gap-10
+            h-full w-72 lg:w-100 flex flex-col gap-10 lg:gap-10
         ${
           isOpen ? "translate-x-0" : "translate-x-[-100%]"
         } lg:translate-x-0 lg:relative`}
